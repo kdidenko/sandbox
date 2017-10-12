f2c22008f753d348d04f193fa081762f8fc4d68c
itodemo <demo@ito-global.com>
2017-10-12 17:40:12 +0100

    initial extension's dev environment code commit

---
+++ app/scripts/tabsavergui.js
@@ -0,0 +1,16 @@
+console.log('Welcome to TabSaver AMD GUI app!');
+
+(function(){
+	'use strict';
+	
+	angular
+		.module('TabSaverGUI',['ngMaterial', 'ngMessages'])
+		.controller('GUICtrl', GUICtrl);
+		
+	
+	function GUICtrl ($scope) {
+		
+	}
+	
+	
+})();
